71c4adfb5f9856720ad30cf000428430e6bccc1a
Hellsingi <il.dzemchanka@gmail.com>
2021-06-20 20:25:13 +0300

    fix: docker compose file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import app from './app';
 import { PORT } from './common/config';
-// import { connectToDB } from './db/connect';
+import { connectToDB } from './db/connect';
 
-// connectToDB();
+connectToDB();
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
