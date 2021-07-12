5676afce9281cf4e4b8469fc0f1e66c1444f3ca7
Hellsingi <il.dzemchanka@gmail.com>
2021-07-12 22:20:22 +0300

    feat: update logic for middleware

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import { log } from './winston-logger';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    log.info(JSON.stringify({
+      METHOD: req.method,
+      URL: req.url,
+      STATUS: res.statusCode,
+      QUERY: req.query,
+      PARAMS: req.params,
+      BODY: req.body
+    }));
+    next();
+  }
+}
