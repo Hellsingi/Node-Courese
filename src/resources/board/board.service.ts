b46578dc87c83dce090e8a2ea0d28abc427ed3ff
Hellsingi <il.dzemchanka@gmail.com>
2021-07-12 22:14:08 +0300

    feat: transfer login to nest

--- src/resources/board/board.service.ts
+++ src/resources/board/board.service.ts
@@ -2,16 +2,15 @@ import boardsRepo from './board.memory.repository';
 import Board from '../../entity/board.entity';
 
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
 export class BoardService {
-  getAll = ():Promise<Board[]> => boardsRepo.getAll();
+  getAll = (): Promise<Board[]> => boardsRepo.getAll();
 
-  createBoard = (board: Board):Promise<Board> => boardsRepo.createBoard(board);
+  createBoard = (board: Board): Promise<Board> => boardsRepo.createBoard(board);
 
-  getById = (id: number):Promise<number | Board | undefined> => boardsRepo.getById(id);
+  getById = (id: number): Promise<number | Board | undefined> => boardsRepo.getById(id);
 
-  putById = (newBoard: Board, id: number):Promise<Board | undefined> => boardsRepo.putById(newBoard, id);
+  putById = (newBoard: Board, id: number): Promise<Board | undefined> => boardsRepo.putById(newBoard, id);
 
-  deleteById = (id:number):Promise<number> => boardsRepo.deleteById(id);
+  deleteById = (id: number): Promise<number> => boardsRepo.deleteById(id);
 }
